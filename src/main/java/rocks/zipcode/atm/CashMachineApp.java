6e840214992b6b627cd69dd93eb9e86584a5b68f
Giles Bradford <gbradford@TommyRogers.localdomain>
2020-02-21 20:27:24 -0500

    saving

--- src/main/java/rocks/zipcode/atm/CashMachineApp.java
+++ src/main/java/rocks/zipcode/atm/CashMachineApp.java
@@ -1,5 +1,7 @@
 package rocks.zipcode.atm;
 
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import rocks.zipcode.atm.bank.Bank;
 import javafx.application.Application;
 import javafx.scene.Parent;
@@ -18,6 +20,48 @@ public class CashMachineApp extends Application {
 
     private TextField field = new TextField();
     private CashMachine cashMachine = new CashMachine(new Bank());
+
+    private Parent createLoginPage() {
+
+        TextField field = new TextField();
+        
+        VBox vbox = new VBox(10);
+        vbox.setPrefSize(600, 600);
+
+        Button btnLogin = new Button("Login");
+        /*btnLogin.setPrefSize(100, 75);
+        btnLogin.setTranslateX(200);
+        btnLogin.setTranslateY(100);*/
+        btnLogin.setOnAction(e -> {
+            int id = Integer.parseInt(field.getText());
+            cashMachine.login(id);
+
+            //areaInfo.setText(cashMachine.toString());
+        });
+
+        Button btnCreate = new Button("Create Account");
+        /*btnCreate.setPrefSize(100, 75);
+        btnLogin.setTranslateX(200);
+        btnLogin.setTranslateY(100);*/
+        btnCreate.setOnAction(e -> {
+            int id = Integer.parseInt(field.getText());
+            cashMachine.login(id);
+
+            //areaInfo.setText(cashMachine.toString());
+        });
+
+        FlowPane flowpane = new FlowPane();
+
+        flowpane.getChildren().add(btnLogin);
+        flowpane.getChildren().add(btnCreate);
+
+        vbox.getChildren().addAll(flowpane, field);
+        return vbox;
+    }
+
+
+
+
 
     private Parent createContent() {
         VBox vbox = new VBox(10);
@@ -77,7 +121,7 @@ public class CashMachineApp extends Application {
 
     @Override
     public void start(Stage stage) throws Exception {
-        stage.setScene(new Scene(createContent()));
+        stage.setScene(new Scene(createLoginPage()));
         stage.show();
     }
 
