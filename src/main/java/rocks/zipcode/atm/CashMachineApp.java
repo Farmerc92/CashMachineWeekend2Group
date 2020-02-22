45120b8604b093e0c596a4e10aa366e49f364dc8
Giles Bradford <gbradford@TommyRogers.localdomain>
2020-02-21 23:02:30 -0500

    working UI, not complete yet. But basics are known.

--- src/main/java/rocks/zipcode/atm/CashMachineApp.java
+++ src/main/java/rocks/zipcode/atm/CashMachineApp.java
@@ -2,6 +2,7 @@ package rocks.zipcode.atm;
 
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
+import javafx.scene.layout.StackPane;
 import rocks.zipcode.atm.bank.Bank;
 import javafx.application.Application;
 import javafx.scene.Parent;
@@ -18,110 +19,54 @@ import javafx.scene.layout.FlowPane;
  */
 public class CashMachineApp extends Application {
 
-    private TextField field = new TextField();
-    private CashMachine cashMachine = new CashMachine(new Bank());
-
-    private Parent createLoginPage() {
-        VBox vbox = new VBox(10);
-        vbox.setPrefSize(600, 600);
-
-        TextField field = new TextField();
-        TextArea areaInfo = new TextArea();
-
-        Button btnLogin = new Button("Login");
-        btnLogin.setOnAction(e -> {
-            int id = Integer.parseInt(field.getText());
-            cashMachine.login(id);
-
-            areaInfo.setText(cashMachine.toString());
-        });
-
-        Button btnCreate = new Button("Create Account");
-        btnCreate.setOnAction(e -> {
-            int id = Integer.parseInt(field.getText());
-            cashMachine.createAccount(id);
-
-            //areaInfo.setText(cashMachine.toString());
-        });
-
-        FlowPane flowpane = new FlowPane();
-
-        flowpane.getChildren().add(btnLogin);
-        flowpane.getChildren().add(btnCreate);
-
-        vbox.getChildren().addAll(flowpane, field, areaInfo);
-        return vbox;
-    }
-
-
-
-
-
-    private Parent createContent() {
-        VBox vbox = new VBox(10);
-        vbox.setPrefSize(600, 600);
-
-        TextArea areaInfo = new TextArea();
-
-        Button btnSubmit = new Button("Set Account ID");
-        btnSubmit.setOnAction(e -> {
-            int id = Integer.parseInt(field.getText());
-            cashMachine.login(id);
-
-            areaInfo.setText(cashMachine.toString());
-        });
-
-        Button btnPin = new Button("Enter Pin");
-        btnSubmit.setOnAction(e -> {
-            int id = Integer.parseInt(field.getText());
-            cashMachine.login(id);
-
-            areaInfo.setText(cashMachine.toString());
-        });
-
-        Button btnDeposit = new Button("Deposit");
-        btnDeposit.setOnAction(e -> {
-            int amount = Integer.parseInt(field.getText());
-            cashMachine.deposit(amount);
-
-            areaInfo.setText(cashMachine.toString());
-        });
-
-        Button btnWithdraw = new Button("Withdraw");
-        btnWithdraw.setOnAction(e -> {
-            int amount = Integer.parseInt(field.getText());
-            cashMachine.withdraw(amount);
-
-            areaInfo.setText(cashMachine.toString());
-        });
-
-        Button btnExit = new Button("Exit");
-        btnExit.setOnAction(e -> {
-            cashMachine.exit();
-
-            areaInfo.setText(cashMachine.toString());
-        });
-
-        FlowPane flowpane = new FlowPane();
-
-        flowpane.getChildren().add(btnSubmit);
-        flowpane.getChildren().add(btnPin);
-        flowpane.getChildren().add(btnDeposit);
-        flowpane.getChildren().add(btnWithdraw);
-        flowpane.getChildren().add(btnExit);
-        vbox.getChildren().addAll(field, flowpane, areaInfo);
-        return vbox;
-    }
-
-    //Scene scene1, scene2;
-
-    @Override
-    public void start(Stage stage) throws Exception {
-        stage.setScene(new Scene(createLoginPage()));
-        stage.show();
-    }
+    Stage window;
+    Scene scene1, scene2;
 
     public static void main(String[] args) {
         launch(args);
     }
+
+    @Override
+    public void start(Stage mainStage) throws Exception {
+        window = mainStage;
+
+
+        /*Scene1 * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+        TextField textField = new TextField();
+        FlowPane flowpane = new FlowPane();
+        Button btnLogin = new Button("Login");
+        btnLogin.setOnAction(e -> window.setScene(scene2));
+        Button btnCreateAccount = new Button("Create Account");
+        TextArea areaInfo = new TextArea();
+
+        VBox vbox = new VBox(10);
+        scene1 = new Scene (vbox, 600, 600);
+
+        flowpane.getChildren().add(btnLogin);
+        flowpane.getChildren().add(btnCreateAccount);
+        vbox.getChildren().addAll(textField, flowpane, areaInfo, btnLogin, btnCreateAccount);
+        /* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+
+
+        /*Scene2 * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+        Button btnCreate = new Button("Create");
+        Button btnSubmit = new Button("Go Back");
+        btnSubmit.setOnAction(e -> window.setScene(scene1));
+        TextField idField = new TextField();
+        TextField passwordField = new TextField();
+
+
+        StackPane vbox2 = new StackPane();
+        flowpane.getChildren().add(btnCreate);
+        flowpane.getChildren().add(btnSubmit);
+        vbox2.getChildren().addAll(flowpane, idField, passwordField);
+        scene2 = new Scene(vbox2, 600, 600);
+        /* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+
+
+        mainStage.setScene(scene1);
+        mainStage.show();
+
+    }
+
 }
