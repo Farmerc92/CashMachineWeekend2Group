22cc30d7aa09cc31287343d3fc6836e6fc678e7f
Giles Bradford <gbradford@TommyRogers.localdomain>
2020-02-22 10:57:23 -0500

    saving

--- src/main/java/rocks/zipcode/atm/CashMachineApp.java
+++ src/main/java/rocks/zipcode/atm/CashMachineApp.java
@@ -1,7 +1,5 @@
 package rocks.zipcode.atm;
 
-import javafx.geometry.Insets;
-import javafx.geometry.Pos;
 import javafx.scene.layout.StackPane;
 import rocks.zipcode.atm.bank.Bank;
 import javafx.application.Application;
@@ -13,6 +11,7 @@ import javafx.scene.control.TextField;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 import javafx.scene.layout.FlowPane;
+import javafx.scene.text.Text;
 
 /**
  * @author ZipCodeWilmington
@@ -20,7 +19,7 @@ import javafx.scene.layout.FlowPane;
 public class CashMachineApp extends Application {
 
     Stage window;
-    Scene scene1, scene2;
+    Scene scene1, scene2, scene3;
 
     public static void main(String[] args) {
         launch(args);
@@ -30,13 +29,14 @@ public class CashMachineApp extends Application {
     public void start(Stage mainStage) throws Exception {
         window = mainStage;
 
-
+        /*Scene1 * * * * * * * * LOGIN OR CREATE ACCOUNT PAGE * * * * * * * * * * * * * */
         /*Scene1 * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
         TextField textField = new TextField();
         FlowPane flowpane = new FlowPane();
         Button btnLogin = new Button("Login");
-        btnLogin.setOnAction(e -> window.setScene(scene2));
+        btnLogin.setOnAction(a -> window.setScene(scene3));
         Button btnCreateAccount = new Button("Create Account");
+        btnCreateAccount.setOnAction(e -> window.setScene(scene2));
         TextArea areaInfo = new TextArea();
 
         VBox vbox = new VBox(10);
@@ -44,23 +44,49 @@ public class CashMachineApp extends Application {
 
         flowpane.getChildren().add(btnLogin);
         flowpane.getChildren().add(btnCreateAccount);
-        vbox.getChildren().addAll(textField, flowpane, areaInfo, btnLogin, btnCreateAccount);
+        vbox.getChildren().addAll(btnLogin, btnCreateAccount, textField, flowpane, areaInfo);
         /* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
 
 
+        /*Scene2 * * * * * * * * CREATE ACCOUNT PAGE * * * * * * * * * * * * * * * * * * */
         /*Scene2 * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
-        Button btnCreate = new Button("Create");
+        Button btnCreate = new Button("Submit");
         Button btnSubmit = new Button("Go Back");
         btnSubmit.setOnAction(e -> window.setScene(scene1));
-        TextField idField = new TextField();
-        TextField passwordField = new TextField();
+
+        Text newName = new Text();
+        newName.setText("Please enter your name:");
+        TextField nameField = new TextField();
+
+        Text newEmail = new Text();
+        newEmail.setText("Please enter your email:");
+        TextField emailField = new TextField();
+        
+        Text newPin = new Text();
+        newPin.setText("Please enter your desired pin:");
+        TextField pinField = new TextField();
 
 
-        StackPane vbox2 = new StackPane();
+        VBox vbox2 = new VBox();
+        scene2 = new Scene(vbox2, 600, 600);
         flowpane.getChildren().add(btnCreate);
         flowpane.getChildren().add(btnSubmit);
-        vbox2.getChildren().addAll(flowpane, idField, passwordField);
-        scene2 = new Scene(vbox2, 600, 600);
+        vbox2.getChildren().addAll(newName, nameField, newEmail, emailField, newPin, pinField, flowpane, btnCreate, btnSubmit);
+        /* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+
+
+        /*Scene3 * * * * * * * * * * MAIN PAGE AFTER LOGIN * * * * * * * * * * * * * * * */
+        /*Scene3 * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+        Button btnSubmitCredentials = new Button("Test");
+        Button btnGoBack = new Button("testAgain");
+        btnGoBack.setOnAction(e -> window.setScene(scene1));
+
+
+        VBox vbox3 = new VBox();
+        flowpane.getChildren().add(btnSubmitCredentials);
+        flowpane.getChildren().add(btnGoBack);
+        vbox3.getChildren().addAll(flowpane, btnSubmitCredentials, btnGoBack);
+        scene3 = new Scene(vbox3, 600, 600);
         /* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
 
 
